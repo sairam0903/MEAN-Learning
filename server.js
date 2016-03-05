728ef2e462f77959140e0c2015e99ec37c640b89
schittimilla <schittimilla@ebay.com>
2016-03-05 15:39:04 -0800

    reverting the previous commit

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ var express = require('express'),
 
 app
     .use(express.static('./node_modules'))
-    .use(express.static('./bower_components'))
     .use(express.static('./public'))
     .use('/api', api)
     .get('*', function (req, res) {
         res.sendFile('main.html',{'root':__dirname + '/public'});
     })
-    .listen(9001, function(){
-        console.log('Server running at http://localhost:9001 !!')
+    .listen(8000, function(){
+        console.log('Server running at http://localhost:8000 !!')
     });
